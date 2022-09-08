4b8cd12af70a0f9882e9d64e785221ea1c71157c
Colby <me@colbymchenry.com>
2022-09-08 10:02:18 -0400

    Email balst.

--- src/routes/api/blast.js
+++ src/routes/api/blast.js
@@ -1,27 +1,32 @@
 import {FirebaseAdmin} from "../../utils/firebase/FirebaseAdmin.js";
 import {SMSHelper} from "../../utils/SMSHelper.js";
+import {MailHelper} from "../../utils/MailHelper.js";
 
 export async function get({request, url}) {
     await FirebaseAdmin.auth().verifyIdToken(request.headers.get("authorization"));
 
     const clients = await FirebaseAdmin.getCollectionArray("clients");
     let failed = [];
+    let success = [];
 
-    // await Promise.all(clients.map(async (client) => {
-    //     if (client?.phoneNumber) {
-    //         try {
-    //             await SMSHelper.sendText(client.phoneNumber,
-    //                 `Balanced Aesthetics: New Revolutionary FDA Approved Weight Loss Supplement Semaglutide is Changing Lives!\n\nGet 2 Weeks On Us When You Order 2 Months or More!\n\nOur Patients on Average See 21% Weight Loss. No Restrictive Diet or Intense Exercise!\n\nSchedule Your Consultation Today!\n\n470-205-4019`
-    //             )
-    //         } catch (error) {
-    //             failed.push(client);
-    //             console.error(error);
-    //         }
-    //
-    //     }
-    // }))
+    await Promise.all(clients.map(async (client) => {
+        if (client?.email) {
+            try {
+                // await SMSHelper.sendText(client.phoneNumber,
+                //     `Balanced Aesthetics: New Revolutionary FDA Approved Weight Loss Supplement Semaglutide is Changing Lives!\n\nGet 2 Weeks On Us When You Order 2 Months or More!\n\nOur Patients on Average See 21% Weight Loss. No Restrictive Diet or Intense Exercise!\n\nSchedule Your Consultation Today!\n\n470-205-4019`
+                // )
+                await MailHelper.send(client.email, "New Revolutionary FDA Approved Weight Loss Supplement Semaglutide");
+                success.push(client.email);
+            } catch (error) {
+                failed.push(client?.email);
+                console.error(error);
+            }
+
+        }
+    }))
 
     console.log("FAILED", failed);
+    console.log("SUCCESS", failed);
 
     return {
         status: 200,
